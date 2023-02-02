f1415a36debda773634ee3171c59236a42c627a0
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-02-02 18:16:17 -0500

    Front corregido

--- src/main/resources/static/assets/js/home.js
+++ src/main/resources/static/assets/js/home.js
@@ -78,6 +78,9 @@ const app = createApp({
                 currency: 'USD',
             });
             return USDollar.format(aux)
+        },
+        redirect() {
+            location.href = '../../store.html'
         }
     }
 })
